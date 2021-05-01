bd89382eeeadc382a7af4e131746bf062fa31b7b
Adam <adamgirach@gmail.com>
2021-05-01 15:41:13 +0100

    Added /snack, /tag and /review to url endpoints, added get all query for snack, tag and review class-views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from snacks import views
+
+router = routers.DefaultRouter()
+router.register(r'snack', views.SnackView, 'snack')
+router.register(r'tag', views.TagView, 'tag')
+router.register(r'review', views.ReviewView, 'review')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/snacks/views.py
+++ backend/snacks/views.py
@@ -9,67 +9,79 @@ from rest_framework import viewsets, permissions, filters
 from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
-class SnackView(View):
-    model = Snack
-    
-    def snack(request):
-        if request.method == 'GET':
-            if len(request.GET['id']) == 1: # check if single id parameter is given in request [39]
-                # get snack from id parameter
-                query = request.query_params.get('id')
-                snack = Snack.objects.get(id=query)
-                # return snack as JSON
-                if snack: # check if database result contains data
-                    return JsonResponse(snack.values()) # return single snack
-                else:
-                    return Response(status=HTTP_400_BAD_REQUEST, data=error)
-            else: # if no id given then return all snacks
-                snacks = Snack.objects.all()
-                return JsonResponse(list(snacks.values()))
-        else:
-            return Response(status=HTTP_400_BAD_REQUEST, data=error)
-
-    @login_required
-    def new_snack(request): 
-        if request.method =='POST':
-            pass # Replace this line with code for adding new snack
-        else:
-            return Response(status=HTTP_400_BAD_REQUEST, data=error)    
-    
-
-
-class SnackViewSet(viewsets.ModelViewSet):
-    queryset = Snack.objects.all()
+class SnackView(viewsets.ModelViewSet):
     serializer_class = SnackSerializer
+    queryset = Snack.objects.all()
 
-    # permissision_classes = [
-    #     permissions.IsAuthenticatedOrReadOnly
-    # ]
+class TagView(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
+class ReviewView(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
+
+# class SnackView(View):
+#     model = Snack
+    
+#     def snack(request):
+#         if request.method == 'GET':
+#             if len(request.GET['id']) == 1: # check if single id parameter is given in request [39]
+#                 # get snack from id parameter
+#                 query = request.query_params.get('id')
+#                 snack = Snack.objects.get(id=query)
+#                 # return snack as JSON
+#                 if snack: # check if database result contains data
+#                     return JsonResponse(snack.values()) # return single snack
+#                 else:
+#                     return Response(status=HTTP_400_BAD_REQUEST, data=error)
+#             else: # if no id given then return all snacks
+#                 snacks = Snack.objects.all()
+#                 return JsonResponse(list(snacks.values()))
+#         else:
+#             return Response(status=HTTP_400_BAD_REQUEST, data=error)
+
+#     @login_required
+#     def new_snack(request): 
+#         if request.method =='POST':
+#             pass # Replace this line with code for adding new snack
+#         else:
+#             return Response(status=HTTP_400_BAD_REQUEST, data=error)    
+    
 
 
-    #filter_backends = [IsOwnerOrAnonFilterBackend, DjangoFilterBackend,
-    #                   filters.OrderingFilter, filters.SearchFilter]
-    #filterset_fields = ["subject__name", "answer"]
+# class SnackViewSet(viewsets.ModelViewSet):
+#     queryset = Snack.objects.all()
+#     serializer_class = SnackSerializer
 
-    #ordering_fields = ["rating"] (need to aggregate rating before any sorting)
-    #search_fields = ["question", ]
+#     # permissision_classes = [
+#     #     permissions.IsAuthenticatedOrReadOnly
+#     # ]
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
 
-    @action(detail=True)
-    def answered(self, request, pk=None):
-        card = self.get_object()
-        val = card.difficulty
-        outcome = self.request.query_params.get('correct')
-        serializer = self.get_serializer(card)
+#     #filter_backends = [IsOwnerOrAnonFilterBackend, DjangoFilterBackend,
+#     #                   filters.OrderingFilter, filters.SearchFilter]
+#     #filterset_fields = ["subject__name", "answer"]
 
-        if outcome == "true":
-            val += 1
-        elif outcome == "false":
-            val -= 2
-        else:
-            pass
-        card.difficulty = (val)
-        card.save()
-        return Response(serializer.data)
+#     #ordering_fields = ["rating"] (need to aggregate rating before any sorting)
+#     #search_fields = ["question", ]
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+#     @action(detail=True)
+#     def answered(self, request, pk=None):
+#         card = self.get_object()
+#         val = card.difficulty
+#         outcome = self.request.query_params.get('correct')
+#         serializer = self.get_serializer(card)
+
+#         if outcome == "true":
+#             val += 1
+#         elif outcome == "false":
+#             val -= 2
+#         else:
+#             pass
+#         card.difficulty = (val)
+#         card.save()
+#         return Response(serializer.data)

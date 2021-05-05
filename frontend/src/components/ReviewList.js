af87a4dd762a1d94c77eef6d5415b7bc26f7bb59
Adam <adamgirach@gmail.com>
2021-05-05 19:01:31 +0100

    No need for fetch request anymore, reviews prop passed down from snack detail, changed 'description' label to 'comment'

--- frontend/src/components/ReviewList.js
+++ frontend/src/components/ReviewList.js
@@ -1,18 +1,6 @@
-import { useState, useEffect } from "react";
-import { reviewsAPI } from "../api/reviews";
 import "../static/review.css"
 
-function ReviewList({ snack_id }) {
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    reviewsAPI.getReviews(snack_id).then(
-      (reviews) => {
-        setReviews(reviews);
-      },
-      (error) => console.log(error)
-    );
-  }, [snack_id]);
+function ReviewList({ reviews }) {
 
   return (
     <>
@@ -23,7 +11,7 @@ function ReviewList({ snack_id }) {
             <thead>
               <tr>
                 <th>Name</th>
-                <th>Description</th>
+                <th>Comment</th>
                 <th>Rating</th>
                 <th>Date</th>
                 <th />

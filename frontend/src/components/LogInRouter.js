d89c05e50dcaa5b60d15744a7e7d16cbad79fb8e
Adam <adamgirach@gmail.com>
2021-05-06 19:48:20 +0100

    Fixed missing props for login

--- frontend/src/components/LogInRouter.js
+++ frontend/src/components/LogInRouter.js
@@ -1,26 +1,8 @@
 import { useParams, Redirect } from "react-router-dom";
-import { useState, useEffect } from "react";
 import SignIn from "./SignIn.js";
-import { accountsAPI } from "../api/accounts";
 
-function LogInRouter ({ loggedIn, onSubmit }) {
+function LogInRouter ({ loggedIn, onSubmit, handleUsernameChange, handlePasswordChange }) {
     const prev_snack = useParams();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    // const onClick = (e) => {
-    //     e.preventDefault();
-    //     setUsername("");
-    //     setPassword("");
-    //   };
-
-      const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-      };
-    
-      const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-      };
 
     if (prev_snack["id"] && loggedIn) {
         return <Redirect exact to={"/snack/" + prev_snack["id"]} />
